733d0c99908d902dc646404d54c71caf9d636086
Jni <jni.viens@gmail.com>
2023-11-06 18:47:02 -0500

    Fixes line endings in JSON on windows

--- src/main/kotlin/ca/ulaval/glo4002/codereview/app/infra/json/ObjectMapper.kt
+++ src/main/kotlin/ca/ulaval/glo4002/codereview/app/infra/json/ObjectMapper.kt
@@ -1,10 +1,11 @@
 package ca.ulaval.glo4002.codereview.app.infra.json
 
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 val configuredMapper = ObjectMapper().apply {
     registerKotlinModule()
-    enable(SerializationFeature.INDENT_OUTPUT)
 }
